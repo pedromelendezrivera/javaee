b44e25bd99e527b222c5ca2b93df3079797d282b
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-05-22 10:07:57 -0500

    Update ControladorProducto.java

--- workshop servlet 02/controlador/ControladorProducto.java
+++ workshop servlet 02/controlador/ControladorProducto.java
@@ -153,6 +153,44 @@ public class ControladorProducto extends HttpServlet {
 
     
     
+     
+     
+     
+     
+     
+     
+     
+     
+                 //leer la informacion que viene del formulario
+             String CodArticulo = request.getParameter("cArt");
+             String seccion = request.getParameter("seccion");
+             String NombreArticulo = request.getParameter("nArt");
+             Date fecha = null;
+            SimpleDateFormat formatofecha = new SimpleDateFormat("yyyy-MM-dd");
+             try {
+             fecha = formatofecha.parse(request.getParameter("fecha"));
+             } catch (ParseException ex) {
+                ex.printStackTrace();
+              }        
+            double Precio = Double.parseDouble(request.getParameter("precio"));
+            int Importado = Integer.parseInt(request.getParameter("importado"));
+            String PaisOrigen = request.getParameter("pOrig");
+
+            // crear un objeto de tipo producto
+            Productos nuevoProducto = new Productos(CodArticulo,seccion,NombreArticulo,Precio,fecha,Importado,PaisOrigen,null);      
+               
+         try {
+             modeloProductos.agregarProducto(nuevoProducto);
+         } catch (SQLException ex) {
+            ex.printStackTrace();
+         }
+             //volver a listar productos
+             obtenerProductos(request,response);  
+     
     
+     
+     
+     
+     
     
 }
