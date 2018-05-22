2214437f4b81cc56e16a1cf5537c3baa08e4c8cf
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-05-22 10:54:20 -0500

    Update ControladorProducto.java

--- workshop servlet 02/controlador/ControladorProducto.java
+++ workshop servlet 02/controlador/ControladorProducto.java
@@ -1,30 +1,15 @@
 
-            try {
-                 //leer los datos del formulario actualizaProductov2
-                 String CodArticulo = request.getParameter("cArt");
-                 String seccion = request.getParameter("seccion");
-                 String NombreArticulo = request.getParameter("nArt");
-                 Date fecha = null;
-                 SimpleDateFormat formatofecha = new SimpleDateFormat("yyyy-MM-dd");
-                 try {
-                     fecha = formatofecha.parse(request.getParameter("fecha"));
-                 } catch (ParseException ex) {
-                     ex.printStackTrace();
-                 }
-                 double Precio = Double.parseDouble(request.getParameter("precio"));
-                 int Importado = Integer.parseInt(request.getParameter("importado"));
-                 String PaisOrigen = request.getParameter("pOrig");
-                 
-                 //crear un objeto de tipo producto con la informacion del formulario
-                 Productos productoActualizado = new Productos(CodArticulo,seccion,NombreArticulo,Precio,fecha,Importado,PaisOrigen,null);
-                 
-                 //actualizar la BBDD con la informacion del objeto producto
-                 modeloProductos.actualizarProducto(productoActualizado);
-                 
-                 //Volver a listar con la informacion actualizada
-                 obtenerProductos(request,response);
-                 
-             } catch (SQLException ex) {
-                ex.printStackTrace();
-              } 
- 
+
+ <!--LINK´PARA CADA PRODUCTO CON SU CAMPO CLAVE actualizar -->   
+         <c:url var="linkTemp" value="ControladorProducto">
+             <c:param name="instruccion" value="cargar"></c:param>    
+             <c:param name="CArticulo" value="${tempProd.cArt}"></c:param>                 
+         </c:url>  
+   
+        <!--LINK´PARA CADA PRODUCTO CON SU CAMPO CLAVE eliminar -->   
+         <c:url var="linkTempEliminar" value="ControladorProducto">
+             <c:param name="instruccion" value="eliminarProducto"></c:param>    
+             <c:param name="CArticulo" value="${tempProd.cArt}"></c:param>                 
+         </c:url>           
+         
+                     
